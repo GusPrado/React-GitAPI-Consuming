8d45195c70bca9d80430b2c9f2297aa16b6ba187
Gus Prado <contato@gusprado.com>
2019-11-19 10:55:26 -0300

    Added error handling filters & pagination

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -12,13 +12,16 @@ export default class Main extends Component {
         newRepo: '',
         repositories: [],
         loading: false,
+        error: null,
     };
 
     // Carregar os dados do localStorage
     componentDidMount() {
         const repositories = localStorage.getItem('repositories');
         if (repositories) {
-            this.setState({ repositories: JSON.parse(repositories) });
+            this.setState({
+                repositories: JSON.parse(repositories),
+            });
         }
     }
 
@@ -31,64 +34,84 @@ export default class Main extends Component {
     }
 
     handleInputChange = event => {
-        this.setState({ newRepo: event.target.value });
+        this.setState({
+            newRepo: event.target.value,
+            error: null,
+        });
     };
 
     handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ loading: true });
-        const { newRepo, repositories } = this.state;
-        const res = await api.get(`/repos/${newRepo}`);
-        const data = {
-            name: res.data.full_name,
-        };
-
         this.setState({
-            repositories: [...repositories, data],
-            newRepo: '',
-            loading: false,
+            loading: true,
+            error: false,
         });
+        try {
+            const { newRepo, repositories } = this.state;
+            if (newRepo === '')
+                throw new Error('Você precisa inserir um repositório');
+            const hasRepo = repositories.find(rep => rep.name === newRepo);
+            if (hasRepo) throw new Error('Repositório duplicado');
+            const res = await api.get(`/repos/${newRepo}`);
+            const data = {
+                name: res.data.full_name,
+            };
+
+            this.setState({
+                repositories: [...repositories, data],
+                newRepo: '',
+            });
+        } catch (error) {
+            this.setState({
+                error: true,
+            });
+        } finally {
+            this.setState({
+                loading: false,
+            });
+        }
     };
 
     render() {
-        const { newRepo, repositories, loading } = this.state;
+        const { newRepo, repositories, loading, error } = this.state;
 
         return (
             <Container>
                 <h1>
                     <FaGithubAlt />
-                    Repositórios
-                </h1>
-                <Form onSubmit={this.handleSubmit}>
+                    Repositórios{' '}
+                </h1>{' '}
+                <Form onSubmit={this.handleSubmit} error={error}>
                     <input
                         type="text"
                         placeholder="Adicionar repositório"
                         value={newRepo}
                         onChange={this.handleInputChange}
-                    />
-
+                    />{' '}
                     <SubmitButton loading={loading}>
+                        {' '}
                         {loading ? (
                             <FaSpinner color="#fff" size={14} />
                         ) : (
                             <FaPlus color="#FFF" size={14} />
-                        )}
-                    </SubmitButton>
-                </Form>
+                        )}{' '}
+                    </SubmitButton>{' '}
+                </Form>{' '}
                 <List>
+                    {' '}
                     {repositories.map(repository => (
                         <li key={repository.name}>
-                            <span>{repository.name}</span>
+                            <span> {repository.name} </span>{' '}
                             <Link
                                 to={`/repository/${encodeURIComponent(
                                     repository.name
                                 )}`}
                             >
-                                Detalhes
-                            </Link>
+                                Detalhes{' '}
+                            </Link>{' '}
                         </li>
-                    ))}
-                </List>
+                    ))}{' '}
+                </List>{' '}
             </Container>
         );
     }
